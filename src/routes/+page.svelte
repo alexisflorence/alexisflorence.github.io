697bcda886ef99380e24653e31f1d3c3cddcd3e3
Alexis Florence <76826364+alexischristine@users.noreply.github.com>
2024-01-25 19:45:08 -0500

    editing page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+  <title>alexisflorence</title>
+</svelte:head>
+<h1 class="text-4xl font-bold mb-2">Alexis Florence</h1>
+<p>Hello. I'm a recent Masters graduate from Northeastern University. Pursuing a career in Software Devlopment</p>
+<div class="text-zinc-400">
+<a href="https://github.com/alexisflorence" class="hover:text-zinc-50">Github</a>
+|
+<a href="hhttps://www.linkedin.com/in/alexis-florence/" class="hover:text-zinc-50">LinkedIn</a>
+</div>
