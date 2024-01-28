5424c4792604b4fe3b1cbc9159af256801567996
Alexis Florence <76826364+alexischristine@users.noreply.github.com>
2024-01-28 15:40:53 -0500

    docs

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,28 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter({
+      // default options are shown
+      pages: 'docs',
+      assets: 'docs',
+      fallback: 'index.html',
+      precompress: false
+    }),
+    // other options
+  }
 };
+
+// export default config;
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+// 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+// 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+// 		adapter: adapter()
+// 	}
+// };
 
 export default config;
